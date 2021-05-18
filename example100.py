90dc2472c0fbb7fd37984613907fc62863db36d5
huangkunb <huangkunb@enn.cn>
2021-05-18 10:56:33 +0800

    to 10

--- example100.py
+++ example100.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+import copy
+import time
 
 # arr[] --> 排序数组
 # low  --> 起始索引
 # high  --> 结束索引
 
+
 class SortClass:
 
     number = 0
-    
+
     def partition(self, arr, low, high):
         i = (low-1)         # 最小元素索引
         pivot = arr[high]  # 取枢纽为最右侧数
 
-        for j in range(low, high): #j用来从左到右遍历数组
+        for j in range(low, high):  # j用来从左到右遍历数组
             # 当前元素小于或等于 pivot
             if arr[j] <= pivot:
-                i = i+1 #将指针向右移一位
+                i = i+1  # 将指针向右移一位
                 '''
                 让指针指向已排序的元素最右位置（下一个位置就大于pivot）
                 此处两种情况：
@@ -25,7 +28,7 @@ class SortClass:
                   下次循环元素如果小于pivot---->i+1之后指向大于pivot的元素，此时交换当前遍历元素(小于pivot)和大于pivot的元素
                   保证指针永远指向已排序数列的最右侧位置
                 '''
-                arr[i], arr[j] = arr[j], arr[i] 
+                arr[i], arr[j] = arr[j], arr[i]
         '''
         一趟循环完成小于pivot的数据都在左侧
         指针指向已排序数据最右
@@ -34,15 +37,15 @@ class SortClass:
         arr[i+1], arr[high] = arr[high], arr[i+1]
         # 指向交换完成的最右
         return (i+1)
-        
+
     # 快速排序函数（单项划分）
     def quickSort(self, arr, low, high):
         if low < high:
 
             pi = self.partition(arr, low, high)
-            #一趟循环完成小于pivot的数据都在左侧
-            #根据pi位置将原数组分割成两个数组
-            #分别递归进行下一次分割
+            # 一趟循环完成小于pivot的数据都在左侧
+            # 根据pi位置将原数组分割成两个数组
+            # 分别递归进行下一次分割
             self.quickSort(arr, low, pi-1)
             self.quickSort(arr, pi+1, high)
         self.number += 1
@@ -123,7 +126,7 @@ class Example:
     """
 
     def ex5(self):
-        l = [5,1,9,3,8,4,7]
+        l = [5, 1, 9, 3, 8, 4, 7]
         # for i in range(7):
         #     x = int(input('integer:\n'))
         #     l.append(x)  # 将三个数放入数组
@@ -131,9 +134,126 @@ class Example:
         SortClass().quickSort(l, 0, (len(l)-1))
         print(l)
 
+    # 实例6
+    """
+    题目：斐波那契数列(面试老问这个)
+    程序分析：0,1,1,2,3,5,8,13,21,34 后边等于前两个数之和
+    """
+
+    def ex6(self, n):  # n第几位斐波那契数列
+        if n == 1 or n == 2:
+            return 1
+        # 此处是递归函数，就是自己调用自己，但是一般这么使用必须要有停止条件，不然会无限制执行
+        return (self.ex6(n-1)+self.ex6(n-2))
+        # 上班的if语句就是这个递归函数的停止条件，依次向前计算，计算到1or2时停止
+
+    # 实例7
+    """
+    题目：将一个列表的数据复制到另一个列表中。
+    程序分析：使用切片、构造函数、copy、*运算符
+    """
+
+    def ex7(self):
+        a = [1, 2, 3]
+        # 切片运算，通过调用a[:]，我们得到一个从列表首端开始到末端的切片，也就是a（指引的列表）的完整复制。但这不是复制列表的唯一方式。看看下面这集中情况的复制：
+        b = a[:]
+        c = list(a)  # list是列表构造函数
+        d = a * 1
+        e = copy.copy(a)
+        # 如果有子列表上述 b/c/d/e 复制的子列表都指向同一对象！！！
+        f = copy.deepcopy(a)  # 对于二维甚至更多维数组，deepcopy是最安全的方法，不管多少层，新列表都是深拷贝
+        print(id(a), a)
+        print(id(b), b)
+        print(id(c), c)
+        print(id(d), d)
+        print(id(e), e)
+        print(id(f), f)
+
+    # 实例8
+    """
+    题目：输出 9*9 乘法口诀表。
+    程序分析：分行与列考虑，共9行9列，i控制行，j控制列。
+    """
+
+    def ex8(self):
+        for i in range(1, 10):
+            print() #控制行，换行
+            for j in range(1, i+1):# 控制列,打印从行开始，第一行1列 第2行两列 …… 所以i+1
+                print("{:d}*{:d}={:d}".format(j, i, i*j),end="\t")
+
+    # 实例9
+    """
+    题目：暂停一秒输出。
+    程序分析：使用 time 模块的 sleep() 函数。
+    """
+    def ex9(self):
+        myD = {'你': '啊', '最': '腻','棒':'加','了':'油'}
+        for key, value in dict.items(myD):
+            print (key, value)
+            time.sleep(1) # 暂停 1 秒
 
 
+    # 实例10
+    """
+    题目：暂停一秒输出，并格式化当前时间。
+    程序分析：使用 time 模块的 sleep() 函数。
+    """
+    def ex10(self):
+        # strfrtime用法
+        # time.strftime(format[,t])
+        # t --- 可选的参数t是一个struct_time对象。
+        # fromat --- 日期时间格式化字符串
+        """
+        指令：
+            %a - 简写的星期几
+            %A - 完整的星期几
+            %b - 缩写的月份名称
+            %B - 完整的月份名称
+            %c - 首选日期和时间表示
+            %C - 世纪值（年份除以100，范围从00到99）
+            %d - 该月的第几天（01?31）
+            %D - 类似 %m/%d/%y
+            %e - 该月的一天（1?31）
+            %g - 类似于%G，但是没有世纪
+            %G - 对应于ISO周数4位数的年份（参见％V）
+            %h - 类似于 %b
+            %H - 小时，使用24小时制（00?23）
+            %I - 小时，使用12小时制（01?12）
+            %j - 一年中的哪一天（001?366）
+            %m - 月份（01?12）
+            %M - 分钟
+            %n - 换行符
+            %p - 根据给定的时间值am或pm
+            %r - 时间在上午和下午的符号：am/pm
+            %R - time in 24 hour notation
+            %S - 秒
+            %t - 制表符
+            %T - 当前时间，等于 %H:%M:%S
+            %u - 工作日为数字（1到7），星期一= 1。警告：在Sun Solaris上周日=1
+            %U - 当年的周数，第一个星期日作为第一周的第一天
+            %V - 本年度ISO 8601的周数（01到53），其中，第1周是在本年度至少4天的第一个星期，星期一作为一周的第一天
+            %W - 当年的周数，与第一个星期一作为第一周的第一天
+            %w - 星期为一个小数，星期日=0
+            %x - 没有时间的日期表示
+            %X - 无日期首选的时间表示
+            %y - 一年无世纪（范围从00到99）
+            %Y - 今年，包括世纪
+            %Z or %z - 时区或名称或缩写
+            %% -文字％字符
+        """
+        print (time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+        
+        # time.localtime--->struct_time
+        # 格式化时间戳为本地时间
 
+        # time.time
+        # 获取时间戳
+
+        # 暂停一秒
+        time.sleep(1)
+
+        print (time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
 
 e = Example()
-e.ex5()
+# print(e.ex6(10))
+e.ex10()
